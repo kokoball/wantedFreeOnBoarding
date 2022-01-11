7a2f1572d0f2a0d0a905d84bfeaf4e18ccc02b66
kokoball <kbp4154@naver.com>
2022-01-11 19:20:29 +0900

    span Badge 추가

--- src/App.jsx
+++ src/App.jsx
@@ -42,30 +42,30 @@ const MainBarNav = styled.nav`
   justify-content: space-between;
 `;
 const Button = styled.button`
-margin: 0;
-padding: 0;
-border: 0;
-background: none;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background: none;
 `;
 
 const ProfileButton = styled.button`
-margin: 0;
-padding: 0;
-border: 0;
-background: none;
-    height: 32px;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-align-items: center;
-    -ms-flex-align: center;
-    align-items: center;
-    -webkit-justify-content: space-between;
-    -ms-flex-pack: justify;
-    justify-content: space-between;
-    vertical-align: top;
-    padding: 0;
-    margin-right: 11px;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background: none;
+  height: 32px;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-justify-content: space-between;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  vertical-align: top;
+  padding: 0;
+  margin-right: 11px;
 `;
 
 const AvatarBorder = styled.div`
@@ -123,10 +123,6 @@ const MenuNameLi = styled.li`
   padding: 15px;
   display: inline-block;
 `;
-const Menu2Img = styled.svg`
-width: 18px;
-height: 18px;
-`;
 
 const Aside = styled.aside`
   padding: 9px 0;
@@ -155,12 +151,21 @@ const AsideUl = styled.ul`
 const AsideLi = styled.li`
   list-style: none;
   position: relative;
-    display: inline-block;
-    height: 100%;
-    vertical-align: middle;
+  display: inline-block;
+  height: 100%;
+  vertical-align: middle;
+`;
+const LeftDivision = styled.div`
+  display: inline-block;
+  content: "";
+  width: 1px;
+  height: 10px;
+  background-color: #e1e2e3;
+  margin: auto 10px;
 `;
 
 const DashboardButton = styled.a`
+  display: inline-block;
   font-size: 13px;
   color: #666;
   line-height: 30px;
@@ -170,6 +175,28 @@ const DashboardButton = styled.a`
   padding: 0 10px;
   margin-left: 15px;
   font-weight: 400;
+`;
+const Badge = styled.span`
+  background-color: rgb(51, 102, 255);
+  width: 13px;
+  height: 13px;
+  position: absolute;
+  top: -8px;
+  left: 25px;
+  padding: 0!important;
+  display: -webkit-inline-flex;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  width: 16px;
+  height: 16px;
+  background: #36f;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  border-radius: 5px;
 `;
 
 export default function App() {
@@ -204,7 +231,7 @@ export default function App() {
                   </Button>
                 </AsideLi>
                 <AsideLi>
-                  <Button style={{ margintop: 'relative' }, { marginTop: '5px' }, { marginRight: '10px' }}>
+                  <Button style={{ margintop: 'relative' }, { marginRight: '10px' }}>
                     <svg xmlns="https://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
                       <defs><path id="bpnpn3yn0a" d="M7.554 14.813h3.183a1.689 1.689 0 01-3.183 0zm1.592 2.25a2.813 2.813 0 002.812-2.813.563.563 0 00-.562-.563h-7.5c-.31 0-.541-.014-.699-.04.018-.036.04-.077.066-.123.036-.065.354-.605.46-.8.477-.875.735-1.676.735-2.599V6.75c0-2.656 2.057-4.688 4.688-4.688 2.63 0 4.687 2.032 4.687 4.688v3.375c0 .923.258 1.724.736 2.6.106.194.424.734.46.799.026.046.047.087.065.123-.157.026-.389.04-.698.04a.564.564 0 000 1.126c1.263 0 1.896-.221 1.896-1.002 0-.26-.092-.494-.28-.833-.045-.083-.361-.619-.456-.792-.395-.724-.598-1.355-.598-2.061V6.75c0-3.28-2.563-5.813-5.812-5.813S3.333 3.47 3.333 6.75v3.375c0 .706-.203 1.337-.598 2.06-.094.174-.41.71-.456.793-.188.339-.279.572-.279.833 0 .78.632 1.002 1.896 1.002H6.39a2.813 2.813 0 002.756 2.25z" /></defs>
                       <g fill="none" fillRule="evenodd">
@@ -222,10 +249,17 @@ export default function App() {
                     <AvatarBorder>
                       <AvatarImg />
                     </AvatarBorder>
+                    <Badge>
+                      <svg className="" width="5" height="5" viewBox="0 0 6 6">
+                        <g fill="#fff" fillRule="nonzero">
+                          <path d="M6.647 11L6.647 7.259 6.688 7.259 9.158 11 11 11 11 5 9.353 5 9.353 8.357 9.322 8.357 7.089 5 5 5 5 11z" transform="translate(-123 -375) translate(20 365) translate(98 5)" />
+                        </g>
+                      </svg>
+                    </Badge>
                   </ProfileButton>
                 </AsideLi>
                 <AsideLi>
-                  |
+                  <LeftDivision />
                   <DashboardButton>기업 서비스</DashboardButton>
                 </AsideLi>
               </AsideUl>
